7607d887ec29fbd9319f50b59759e08d81d5f6e6
Deepak Jangir <tdeepak509@gmail.com>
2023-03-10 12:46:13 +0000

    Implemented Data Ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from logger import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     # Most of the information about the error is stored in the third varirable and hance storing the same
